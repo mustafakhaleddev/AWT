42d6a960c41efa907813a3b628119ea66ec9f07c
Mustafa Khaled <mustafakhaled.dev@gmail.com>
2018-12-05 15:52:07 +0200

    Clean File

--- src/awtBlade.php
+++ src/awtBlade.php
@@ -1,11 +1,10 @@
 <?php
-use mkhdev\AWT\AWTClass;
 return [
     /*
-    |---------------------------------------------------------------------
-    | @awt(string)
-    |---------------------------------------------------------------------
-    */
+     * Register Awt Blade directive
+     * you can use it in blade file
+     * @awt("hello World")
+     */
     'awt' => function ($expression) {
         return  "<?php echo awtTrans($expression) ?>";
     }
